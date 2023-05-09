20ebd8f0bd94770a099fa3573f37f65a64cbcb43
Matteo Montrucchio <matteo.montrucchio99@gmail.com>
2023-05-09 15:15:43 +0200

    setup lab09/05

--- train_classifier.py
+++ train_classifier.py
@@ -146,7 +146,7 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
         source_label = source_label.to(device)
         data = {}
 
-        for clip in range(args.train.num_clips):
+        '''for clip in range(args.train.num_clips):
             # in case of multi-clip training one clip per time is processed
             for m in modalities:
                 data[m] = source_data[m][:, clip].to(device)
@@ -154,7 +154,15 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
             logits, _ = action_classifier.forward(data)
             action_classifier.compute_loss(logits, source_label, loss_weight=1)
             action_classifier.backward(retain_graph=False)
-            action_classifier.compute_accuracy(logits, source_label)
+            action_classifier.compute_accuracy(logits, source_label)'''
+
+        for m in modalities:  # commento by suzie: args.train.num_clips (?)
+            data[m] = source_data[m].to(device)
+
+        logits, _ = action_classifier.forward(data)
+        action_classifier.compute_loss(logits, source_label, loss_weight=1)
+        action_classifier.backward(retain_graph=False)
+        action_classifier.compute_accuracy(logits, source_label)
 
         # update weights and zero gradients if total_batch samples are passed
         if gradient_accumulation_step:
@@ -208,13 +216,19 @@ def validate(model, val_loader, device, it, num_classes):
                 logits[m] = torch.zeros((args.test.num_clips, batch, num_classes)).to(device)
 
             clip = {}
-            for i_c in range(args.test.num_clips):
+            '''for i_c in range(args.test.num_clips):
                 for m in modalities:
                     clip[m] = data[m][:, i_c].to(device)
 
                 output, _ = model(clip)
                 for m in modalities:
-                    logits[m][i_c] = output[m]
+                    logits[m][i_c] = output[m]'''
+            for m in modalities:  # commento by suzie: args.TEST.num_clips (?)
+                clip[m] = data[m].to(device)
+
+            output, _ = model(clip)
+            for m in modalities:
+                logits[m] = output[m]
 
             for m in modalities:
                 logits[m] = torch.mean(logits[m], dim=0)
