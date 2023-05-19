10d3fcd0e32b0b4a2a5617605a0ad057476c7ea9
Matteo Montrucchio <matteo.montrucchio99@gmail.com>
2023-05-19 11:30:47 +0200

    GSD Only and GVD Only

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier
 from models.FinalClassifier2 import ClassifierSourceOnly
+from models.FinalClassifier3 import ClassifierGSD
+from models.FinalClassifier4 import ClassifierGVD
